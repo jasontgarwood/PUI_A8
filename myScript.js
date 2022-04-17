7d9466279837eb026ecf0569aee6ca42a8af2dcd
jasontgarwood <jasontgarwood@gmail.com>
2022-04-17 15:41:40 -0400

    progress

--- myScript.js
+++ myScript.js
@@ -1,6 +1,17 @@
+console.log("working")
 
+$("#navOption").hover( function() {
+    console.log('entered');
+    $(this).css("color","black");
+}, function() {
+    console.log('left');
+    $(this).css("color","gray");
+});
 
-
-
-
-
+$(".coverPhoto").hover( function() {
+    console.log('entered');
+    $(this).css("opacity","100%");
+}, function() {
+    console.log('left');
+    $(this).css("opacity","80%");
+});
