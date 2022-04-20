97cdd1ee55a596599db8e22e33208cce2348fbdc
jasontgarwood <jasontgarwood@gmail.com>
2022-04-20 10:37:52 -0400

    update style

--- myScript.js
+++ myScript.js
@@ -1,11 +1,13 @@
 window.onload = function() {
   var waveText = document.getElementById("waveText");
-  var waveLetters= waveText.children;
-  console.log(waveLetters);
+  if (waveText !== null) {
+    var waveLetters= waveText.children;
+    console.log(waveLetters);
 
-  for (var i = 0; i < waveLetters.length; i++) {
-    var currChild = waveLetters[i];
-    var delay = i/4;
-    currChild.style = "animation-delay:" +delay+ "s;";
+    for (var i = 0; i < waveLetters.length; i++) {
+      var currChild = waveLetters[i];
+      var delay = i/4;
+      currChild.style = "animation-delay:" +delay+ "s;";
+    }
   }
 }
